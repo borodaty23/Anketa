a99391e22b037129ef6efe572cbf9fe77d1cb522
Birillo Ilya <vipilia@mail.ru>
2021-11-12 01:06:05 +0300

    added hm-1 :commit-1

---
+++ anketa.js
@@ -0,0 +1,50 @@
+let getSex = function(){
+    let sex = confirm("Ваш пол - мужской?");
+        if (sex === true){
+    return "Мужской"; 
+    }else{
+    return "Женский";
+    }
+    };
+    
+    let confirmationPensioner = function(){
+    if (gender === "Мужской" && years > 65 ){
+        return "Да";
+    } else  if (gender === "Женский" && years > 55 ) {
+        return "Да";
+    } else {
+    return "Нет";
+    }   
+    
+    }
+    
+    
+    let getFullName = function(){
+    let name = prompt("Введите вашу фамилию имя отчество");
+    while (name === "" || name === null){
+    name = prompt("Вы ДОЛЖНЫ ввести вашу фамилию имя отчество!!!!");
+    }
+    return name;
+    }
+    
+    let getYears = function () {
+    let yearsFull = +prompt("Сколько вам лет?");
+    while (yearsFull !== yearsFull || yearsFull === 0){
+    yearsFull = +prompt("Вы ДОЛЖНЫ указать сколько вам лет цифрами!!!!");
+    }
+    return yearsFull;
+    }
+    
+    let fullName = getFullName();
+    let years = getYears();
+    let gender = getSex()
+    let yearsOnDays = years * 365;
+    let yearsFromFive = years + 5;
+    let pensioner = confirmationPensioner();
+    alert(`Ваше ФИО: ${fullName};
+    Ваш возраст в годах: ${years};
+    Ваш возраст в днях: ${yearsOnDays};
+    Через 5 лет вам будет: ${yearsFromFive};
+    Ваш пол: ${gender};
+    Вы на пенсии: ${pensioner};
+    `)
